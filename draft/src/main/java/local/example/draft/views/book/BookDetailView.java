722a75c6819e27f515c2d55586268f4010317a56
paolomococci <paolomococci@gmail.com>
2021-12-24 17:01:05 +0100

    class BookDetailView belonging to project draft has been edited

--- draft/src/main/java/local/example/draft/views/book/BookDetailView.java
+++ draft/src/main/java/local/example/draft/views/book/BookDetailView.java
@@ -2,6 +2,7 @@ package local.example.draft.views.book;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -149,7 +150,7 @@ public class BookDetailView
                 Notification.show("An exception happened while trying to store the book details.");
             }
         });
-
+        save.addClickShortcut(Key.ENTER);
     }
 
     @Override
