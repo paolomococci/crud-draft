9adf6c2e7a5c12af31b98ab1e97cbf1b0f407f37
paolomococci <paolomococci@gmail.com>
2021-12-26 19:15:40 +0100

    class GuestService belonging to project greeting has been edited

--- greeting/src/main/java/local/example/greeting/data/service/GuestService.java
+++ greeting/src/main/java/local/example/greeting/data/service/GuestService.java
@@ -1,4 +1,19 @@
 package local.example.greeting.data.service;
 
+import local.example.greeting.data.entity.Guest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Optional;
+
 public class GuestService {
+    public Optional<Object> list(PageRequest of) {
+        return null;
+    }
+
+    public Optional<Guest> get(Integer integer) {
+        return null;
+    }
+
+    public void update(Guest guest) {
+    }
 }
