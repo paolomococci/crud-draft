b38fa90eeaf1ea7cff43b7e2a92cb9124ece0c35
paolomococci <paolomococci@gmail.com>
2021-12-28 05:45:00 +0100

    enum Role belonging to project hail has been added

---
+++ hail/src/main/java/local/example/hail/data/Role.java
@@ -0,0 +1,17 @@
+package local.example.hail.data;
+
+public enum Role {
+
+    USER("user"),
+    ADMIN("admin");
+
+    private String roleName;
+
+    private Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
