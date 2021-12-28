ea09bd72b1065c2211b82fc5bf9520a7fd368f8b
paolomococci <paolomococci@gmail.com>
2021-12-28 05:47:38 +0100

    class UserService belonging to project hail has been added

---
+++ hail/src/main/java/local/example/hail/data/service/UserService.java
@@ -0,0 +1,37 @@
+package local.example.hail.data.service;
+
+import java.util.Optional;
+import local.example.hail.data.entity.User;
+import local.example.hail.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    public Optional<User> get(Long id) {
+        return repository.findById(id);
+    }
+
+    public User update(User entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Page<User> list(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public int count() {
+        return (int) repository.count();
+    }
+
+}
