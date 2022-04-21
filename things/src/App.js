10e4df2eeb193432b9cae2efc52407a9f7940fb8
paolomococci <paolomococci@gmail.com>
2022-04-21 06:01:53 +0200

    edited file src/App.js belonging to project things

--- things/src/App.js
+++ things/src/App.js
@@ -11,9 +11,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Router>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div style={{ margin: 10 }}>
+            <p>todo create component</p>
+          </div>
+          <div style={{ margin: 10 }}>
+            <p>todo read component</p>
+          </div>
+          <div style={{ margin: 10 }}>
+            <p>todo update component</p>
+          </div>
+          <div style={{ margin: 10 }}>
+            <p>todo delete component</p>
+          </div>
         </Router>
       </header>
     </div>
