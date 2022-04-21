61e0c38e9a89cf14a404d952bfa9bccaefa2a267
paolomococci <paolomococci@gmail.com>
2022-04-21 07:03:36 +0200

    edited file src/components/create.js belonging to project things

---
+++ things/src/components/create.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import {
+  React,
+  useState
+} from 'react'
+import {
+  Form,
+  Button,
+  Checkbox
+} from 'semantic-ui-react'
+import {
+  useHistory
+} from 'react-router'
