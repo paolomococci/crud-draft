27f4d4ebf43e6a6e223ef82cdbd6e69e4da3b189
paolomococci <paolomococci@gmail.com>
2022-04-21 05:57:04 +0200

    edited file src/App.js belonging to project things

--- things/src/App.js
+++ things/src/App.js
@@ -11,6 +11,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Router>
+          <div></div>
+          <div></div>
+          <div></div>
         </Router>
       </header>
     </div>
