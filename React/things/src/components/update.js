3ae6c31dd8c94368d7896a056ff60d1e466981d6
paolomococci <paolomococci@gmail.com>
2022-04-22 05:21:52 +0200

    complete reorganization of this repository

---
+++ React/things/src/components/update.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+import {
+  React,
+  useState,
+  useEffect
+} from 'react'
+import {
+  Form,
+  Button,
+  Checkbox
+} from 'semantic-ui-react'
+
+import {
+  useNavigate
+} from 'react-router-dom'
+
+function Update() {
+
+  let navigate = useNavigate()
+
+  const [
+    id,
+    setId
+  ] = useState(null)
+
+  const [
+    name,
+    setName
+  ] = useState('')
+
+  const [
+    surname,
+    setSurname
+  ] = useState('')
+
+  const [
+    checked,
+    setChecked
+  ] = useState(false)
+
+  useEffect(
+    () => {
+      setId(localStorage.getItem('ID'))
+      setName(localStorage.getItem('Name'))
+      setSurname(localStorage.getItem('Surname'))
+      setChecked(localStorage.getItem('Checked'))
+    }, []
+  )
+
+  const updateAPIData = () => {
+    axios.put(
+      `http://localhost/sampledata/${id}`,
+      {
+        name,
+        surname,
+        checked
+      }
+    ).then(
+      () => {
+        navigate.push('/read')
+      }
+    )
+  }
+
+  return (
+    <div>
+    <Form className="create-form">
+    <Form.Field>
+      <label>name</label>
+      <input placeholder="name" onChange={(e) => setName(e.target.value)} />
+    </Form.Field>
+    <Form.Field>
+      <label>surname</label>
+      <input placeholder="surname" onChange={(e) => setSurname(e.target.value)} />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="accept terms and conditions" onChange={() => setChecked(!checked)} />
+    </Form.Field>
+    <Button onClick={updateAPIData} type="submit">update</Button>
+    </Form>
+    </div>
+  )
+}
+
+export default Update
