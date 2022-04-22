d639b1510d43c0791741db7735ea003549f9c8db
paolomococci <paolomococci@gmail.com>
2022-04-22 05:58:22 +0200

    complete reorganization of this repository

---
+++ Vaadin/book-collection/src/main/java/local/example/draft/data/service/BookService.java
@@ -0,0 +1,39 @@
+package local.example.draft.data.service;
+
+import java.util.Optional;
+
+import local.example.draft.data.entity.Book;
+import local.example.draft.data.repository.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository repository;
+
+    public Optional<Book> get(Long id) {
+        return repository.findById(id);
+    }
+
+    public Book update(Book entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Page<Book> list(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public int count() {
+        return (int) repository.count();
+    }
+
+}
---
+++ Vaadin/book-collection/src/main/java/local/example/draft/views/hello/HelloView.java
@@ -0,0 +1,41 @@
+package local.example.draft.views.hello;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import javax.annotation.security.RolesAllowed;
+
+import local.example.draft.views.MainLayout;
+
+@PageTitle("Hello")
+@Route(value = "hello", layout = MainLayout.class)
+@RolesAllowed("user")
+public class HelloView
+        extends HorizontalLayout {
+
+    private final TextField nameTextField;
+
+    public HelloView() {
+        nameTextField = new TextField("Please enter your name");
+        Button helloButton = new Button("Hello");
+        helloButton.addClickListener(e -> {
+            Notification.show("Hello " + nameTextField.getValue() + "!");
+            nameTextField.setValue("");
+        });
+        helloButton.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(
+                Alignment.END,
+                nameTextField,
+                helloButton
+        );
+
+        add(nameTextField, helloButton);
+    }
+}
